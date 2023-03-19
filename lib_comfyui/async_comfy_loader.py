a32761c723457abc58b9628288bf621a4e5790ed
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-03-19 17:32:25 -0400

    command argv for comfyui

--- lib_comfyui/async_comfy_loader.py
+++ lib_comfyui/async_comfy_loader.py
@@ -5,17 +5,21 @@ from modules import shared
 import threading
 
 
-def main(model_name_queue):
+def main(cmd_argv, model_name_queue):
     start_update_loop(model_name_queue)
-    start_comfyui()
+    start_comfyui(cmd_argv)
 
 
-def start_comfyui():
+def start_comfyui(cmd_argv):
     comfyui_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ComfyUI')
     sys.path.insert(0, comfyui_path)
-    sys.argv = sys.argv[:1]
-
+    set_comfyui_command_args(cmd_argv)
     runpy.run_path(os.path.join(comfyui_path, "main.py"), {}, '__main__')
+
+
+def set_comfyui_command_args(argv=()):
+    sys.argv = sys.argv[:1]
+    sys.argv.extend(argv)
 
 
 def start_update_loop(model_name_queue):

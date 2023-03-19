003421b55c82b41b282abff3eed8da6946a2e6ef
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-03-19 18:39:31 -0400

    convert comfyui argv

--- lib_comfyui/cmd_opts_map.py
+++ lib_comfyui/cmd_opts_map.py
@@ -5,10 +5,22 @@ def _items(cmd_opts):
     return vars(cmd_opts).items()
 
 
+def is_double_argv(k, v):
+    return not (type(v) is 'bool' and v)
+
+
+def as_argv_list(k, v):
+    result = []
+    comfyui_key = k.replace(comfyui_keyword, '')
+    result.append(comfyui_key)
+    if is_double_argv(comfyui_key, v):
+        result.append(v)
+    return result
+
+
 def convert_arguments(cmd_opts):
-    comfyui_cmd_opts = {}
+    result = []
     for k, v in _items(cmd_opts):
         if comfyui_keyword in k:
-            comfyui_key = k.replace(comfyui_keyword, '')
-            comfyui_cmd_opts[comfyui_key] = v
-    return comfyui_cmd_opts
+            result.extend(as_argv_list(k, v))
+    return result

50a622c787c61699310c0ce19bf968653a88c36f
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-03-20 16:34:59 -0400

    not sure what changed, probably fixed something

--- lib_comfyui/comfyui_tab.py
+++ lib_comfyui/comfyui_tab.py
@@ -5,10 +5,10 @@ import install_comfyui
 from lib_comfyui import comfyui_adapter
 
 
-comfyui_install_instruction_markdown = f'''
+comfyui_install_instructions_markdown = f'''
 ### ComfyUI extension
 It looks like your ComfyUI installation isn't set up yet!  
-Go to settings -> ComfyUI, and set the proper install location.  
+Go to `settings -> ComfyUI`, and set the proper install location.  
   
 Alternatively, if you don't have a ComfyUI install yet, you can (literally) just clic this button to install it automatically: 
 '''
@@ -32,7 +32,7 @@ def verify_comfyui_installation(comfyui_install_location):
 def generate_gradio_component():
     with gr.Blocks() as tab:
         html_component = gr.HTML(comfyui_app_html)
-        default_text = gr.Markdown(comfyui_install_instruction_markdown, visible=False)
+        default_text = gr.Markdown(comfyui_install_instructions_markdown, visible=False)
         automatic_install_button = gr.Button('Install ComfyUI')
         automatic_install_button.click(automatic_install_comfyui, inputs=[], outputs=[], show_progress=True)
         automatic_install_button.visible = False

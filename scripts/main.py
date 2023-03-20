effb79afc4d12ee60cbdfd0800d8dff1a291c228
ljleb <lebel.louisjacob@gmail.com>
2023-03-20 01:31:36 -0400

    ui

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 import modules.scripts as scripts
 import sys
+import gradio as gr
 
 base_dir = scripts.basedir()
 sys.path.append(base_dir)
@@ -19,6 +20,11 @@ class ComfyUIScript(scripts.Script):
         return scripts.AlwaysVisible
 
 
+def on_ui_tabs():
+    root = gr.HTML(f"""<div id="comfyui_webui_container"><object data="http://127.0.0.1:7861" id="comfyui_webui_root"></object></div>""")
+    return [(root, 'ComfyUI', 'comfyui_webui_root')]
+
+
 def on_app_started(*_):
     comfy_adapter.start()
 
@@ -27,5 +33,6 @@ def on_script_unloaded(*_):
     comfy_adapter.stop()
 
 
+script_callbacks.on_ui_tabs(on_ui_tabs)
 script_callbacks.on_app_started(on_app_started)
 script_callbacks.on_script_unloaded(on_script_unloaded)

3c2c3c81ddd544a1af65cd955e5a4a899167c3ab
ljleb <lebel.louisjacob@gmail.com>
2023-03-21 08:10:00 -0400

    refact utils

--- tests/utils.py
+++ tests/utils.py
@@ -2,6 +2,6 @@ import os
 import sys
 
 def setup_test_env():
-    ext_root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    if ext_root not in sys.path:
-        sys.path.append(ext_root)
+    extension_root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    if extension_root not in sys.path:
+        sys.path.append(extension_root)

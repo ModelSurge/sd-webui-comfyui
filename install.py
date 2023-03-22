356778a9b3413d374c83f24462db828cd0f0fb36
ljleb <lebel.louisjacob@gmail.com>
2023-03-22 01:24:58 -0400

    remove test for package.json

--- install.py
+++ install.py
@@ -5,11 +5,10 @@ from modules import shared
 
 
 extension_root = os.path.dirname(os.path.realpath(__file__))
-package_json = os.path.join(extension_root, 'package.json')
 
 
 def install_localtunnel():
-    if shared.cmd_opts.localtunnel_comfyui and not os.path.exists(package_json):
+    if shared.cmd_opts.localtunnel_comfyui:
         subprocess.run('npm install localtunnel', cwd=extension_root, shell=True)
 
 

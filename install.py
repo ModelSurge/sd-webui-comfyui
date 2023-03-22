c29390382d5ba94dde9229691b3832dde9a12cad
ljleb <lebel.louisjacob@gmail.com>
2023-03-22 01:29:45 -0400

    fix install.py

--- install.py
+++ install.py
@@ -1,15 +1,13 @@
 import os
 import subprocess
 import shutil
-from modules import shared
 
 
 extension_root = os.path.dirname(os.path.realpath(__file__))
 
 
 def install_localtunnel():
-    if shared.cmd_opts.localtunnel_comfyui:
-        subprocess.run('npm install localtunnel', cwd=extension_root, shell=True)
+    subprocess.run('npm install', cwd=extension_root, shell=True)
 
 
 npm_executable = shutil.which('npm')

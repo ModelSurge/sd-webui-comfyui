ddfccabea17ae5052e228a9570dc18a8c03500d8
ljleb <set>
2023-07-09 16:18:32 -0400

    listen is str

--- preload.py
+++ preload.py
@@ -1,5 +1,5 @@
 def preload(parser):
-    parser.add_argument("--comfyui-listen", action='store_true', default=None)
+    parser.add_argument("--comfyui-listen", type=str, default=None)
     parser.add_argument("--comfyui-port", type=int, default=8188)
     parser.add_argument("--comfyui-dont-upcast-attention", action='store_true', default=None)
     parser.add_argument("--comfyui-use-split-cross-attention", action='store_true', default=None)

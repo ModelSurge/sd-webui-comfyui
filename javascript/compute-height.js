8701960b1ed900323287e222c6e62c2495542a04
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-07-09 21:25:12 -0400

    remove unused

--- javascript/compute-height.js
+++ javascript/compute-height.js
@@ -1,7 +1,3 @@
-
-
-console.log('adding comfyui event');
-
 function computeComfyuiElementHeight() {
     function getRecursiveParentNode(el, n) {
         if(n <= 0) return el;
@@ -21,40 +17,6 @@ function computeComfyuiElementHeight() {
     // polling ew
     setTimeout(computeComfyuiElementHeight, 200);
 }
-
-
-function loadStyleProxy() {
-    const comfyuiTab = document.getElementById('tab_comfyui_webui_root');
-
-    // making sure the thing is loaded
-    if(comfyuiTab === null || comfyuiTab === undefined) {
-        setTimeout(loadStyleProxy, 200);
-        return;
-    }
-
-    // Create a new MutationObserver instance
-    const styleProxy = new Proxy(comfyuiTab.style, {
-        set: function(target, property, value) {
-            // Perform the original property assignment
-            target[property] = value;
-
-            // Handle the style change
-            console.log("Style has changed for the element!");
-
-            // Additional code to handle the style change goes here
-            computeComfyuiElementHeight();
-
-            // Indicate success
-            return true;
-        }
-    });
-
-    comfyuiTab.style = styleProxy;
-
-    console.log('comfyui event added');
-
-}
-
 
 document.addEventListener("DOMContentLoaded", (e) => {
     computeComfyuiElementHeight();

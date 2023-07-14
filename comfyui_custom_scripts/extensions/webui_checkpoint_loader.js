a862745daae63311d1b16432e10661ad43752507
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-07-14 03:53:20 -0400

    remove tabulations

--- comfyui_custom_scripts/extensions/webui_checkpoint_loader.js
+++ comfyui_custom_scripts/extensions/webui_checkpoint_loader.js
@@ -2,35 +2,35 @@ import { app } from "/scripts/app.js";
 
 
 function addVoidWidget(node, name) {
-	const widget = {
-	    i: 0,
-		type: "customtext",
-		name,
-		get value() {
-		    widget.requestUpdate();
-			return `${widget.i}`;
-		},
-		set value(x) {},
-		requestUpdate() {
-		    widget.i++;
-		}
-	};
-	widget.parent = node;
-	node.addCustomWidget(widget);
+    const widget = {
+        i: 0,
+        type: "customtext",
+        name,
+        get value() {
+            widget.requestUpdate();
+            return `${widget.i}`;
+        },
+        set value(x) {},
+        requestUpdate() {
+            widget.i++;
+        }
+    };
+    widget.parent = node;
+    node.addCustomWidget(widget);
 
-	return widget;
+    return widget;
 }
 
 
 const ext = {
-	name: "sd-webui-comfyui",
-	async getCustomWidgets(app) {
-		return {
+    name: "sd-webui-comfyui",
+    async getCustomWidgets(app) {
+        return {
             VOID(node, inputName) {
                 addVoidWidget(node, inputName);
             },
-		};
-	},
+        };
+    },
 };
 
 app.registerExtension(ext);

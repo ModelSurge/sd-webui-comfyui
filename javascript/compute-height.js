374f4ef26d97917309d7971e539ee8a4e9697b5b
ljleb <set>
2023-07-14 19:26:51 -0400

    new js

--- javascript/compute-height.js
+++ javascript/compute-height.js
@@ -1,25 +1,14 @@
-const TAB_OFFSET_PADDING = 5;
-
-function getRecursiveParentNode(el, n) {
-    if(n <= 0) return el;
-    if(el === null || el.parentNode === null) return el;
-    return getRecursiveParentNode(el.parentNode, n-1);
-}
-
-const getDynamicElementFromContainer = (container) => {
-    const webuiParentDepth = 7;
-    return getRecursiveParentNode(container, webuiParentDepth);
-}
-
 function computeComfyuiElementHeight() {
     const tab = document.getElementById("tab_comfyui_webui_root");
     const container = document.getElementById("comfyui_webui_container");
     const footerToRemove = document.querySelector('#footer');
-    const dynamicElement = getDynamicElementFromContainer(container);
+    const tabs = document.getElementById("tabs") ?? null;
+    const tab_nav = tabs ? tabs.querySelector(".tab-nav") : null;
+    console.log(tab_nav)
 
-    if(dynamicElement !== null) {
-        const height = dynamicElement.offsetHeight;
-        container.style.height = `calc(100% - ${height-TAB_OFFSET_PADDING}px)`;
+    if(tab_nav !== null) {
+        const height = tab_nav.getBoundingClientRect().bottom;
+        container.style.height = `calc(100% - ${height}px)`;
         updateFooterStyle(tab.style.display, footerToRemove);
     }
 

01aa7e75538c0c02d0e040a6278f5c185864cb85
ljleb <set>
2023-07-14 21:56:55 -0400

    cpu state dict

--- lib_comfyui/comfyui_adapter.py
+++ lib_comfyui/comfyui_adapter.py
@@ -11,14 +11,14 @@ model_queue = multiprocessing_spawn.Queue()
 
 def on_model_loaded(sd_model):
     sd_model.share_memory()
-    state_dict = unwrap_state_dict(sd_model.state_dict())
+    state_dict = unwrap_cpu_state_dict(sd_model.state_dict())
     model_queue.put(state_dict)
 
 
-def unwrap_state_dict(state_dict: dict) -> dict:
+def unwrap_cpu_state_dict(state_dict: dict) -> dict:
     model_keys = ('cond_stage_model', 'first_stage_model', 'model.diffusion_model')
     return {
-        k.replace('.wrapped.', '.'): v
+        k.replace('.wrapped.', '.'): v.cpu()
         for k, v in state_dict.items()
         if k.startswith(model_keys)
     }

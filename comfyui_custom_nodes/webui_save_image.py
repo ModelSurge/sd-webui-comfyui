9f5f3ad4cf025a5cf77b5c33c69f9e0d4cf99927
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-07-16 10:50:35 -0400

    basic implementation of a node for saving images to the webui paths

---
+++ comfyui_custom_nodes/webui_save_image.py
@@ -0,0 +1,50 @@
+import os
+
+from PIL import Image
+
+from torchvision.transforms.functional import to_pil_image
+
+from modules.images import save_image
+from modules.shared import opts
+
+
+class WebuiSaveImage:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                    "location": (["txt2img-images", "img2img-images", "extras-images", "txt2img-grids", "img2img-grids", ], ),
+                    "images": ("IMAGE", ),
+            },
+        }
+    RETURN_TYPES = ()
+    FUNCTION = "save_image"
+
+    CATEGORY = "image"
+
+    OUTPUT_NODE = True
+
+    def save_image(self, location, images):
+        webui_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+        if 'txt2img' in location:
+            output_dir = opts.outdir_samples or opts.outdir_txt2img_samples if 'images' in location else opts.outdir_grids or opts.outdir_txt2img_grids
+        elif 'img2img' in location:
+            output_dir = opts.outdir_samples or opts.outdir_img2img_samples if 'images' in location else opts.outdir_grids or opts.outdir_img2img_grids
+        else:
+            output_dir = opts.outdir_samples or opts.outdir_extras_samples
+
+        for image in images:
+            pil_image = to_pil_image(image.permute(2, 0, 1))
+            save_path = os.path.join(webui_path, output_dir)
+            filename, _ = save_image(image=pil_image, path=save_path, basename='')
+
+        return []
+
+
+NODE_CLASS_MAPPINGS = {
+    "WebuiTxt2ImgOutput": WebuiSaveImage,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "WebuiTxt2ImgOutput": 'Webui Save Image',
+}

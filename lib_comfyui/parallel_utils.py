08ef65a0f9c22d6d345396b6a35310147434c519
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-07-16 21:03:25 -0400

    producer handler

--- lib_comfyui/parallel_utils.py
+++ lib_comfyui/parallel_utils.py
@@ -51,7 +51,7 @@ class SynchronizingQueue(multiprocessing.queues.Queue):
         return super(SynchronizingQueue, self).__setstate__(state)
 
 
-class ProducerThreadHandler:
+class ProducerHandler:
     def __init__(self, queue: SynchronizingQueue):
         self.queue = queue
         self.producer_thread = None

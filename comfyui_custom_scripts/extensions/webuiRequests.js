2b0b14ed4c0b4e111ae7b38fffab894e3155b837
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-07-21 07:45:20 -0400

    localStorage

--- comfyui_custom_scripts/extensions/webuiRequests.js
+++ comfyui_custom_scripts/extensions/webuiRequests.js
@@ -86,6 +86,7 @@ function hijackUiEnv(thisClientId) {
     else {
         // ComfyUI has an interval for setting the local storage, but we have many ComfyUI's in parallel, so we restore
         // the prvious correct state here for the tab
+        localStorage.setItem('workflow', startup_workflow);
         setTimeout(() => app.loadGraphData(JSON.parse(startup_workflow)), 500);
     }
 }

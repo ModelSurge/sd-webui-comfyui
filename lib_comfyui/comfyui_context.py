37dde4976bb716c6af337df9592b1d0dc99d85e2
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-07-21 04:08:00 -0400

    fix comfyui context

--- lib_comfyui/comfyui_context.py
+++ lib_comfyui/comfyui_context.py
@@ -5,10 +5,10 @@ base_dir = scripts.basedir()
 
 class ComfyuiContext:
     def __init__(self):
-        self.original_sys_path = list(sys.path)
         self.sys_path_to_add = base_dir
 
     def __enter__(self):
+        self.original_sys_path = list(sys.path)
         sys.path.insert(0, self.sys_path_to_add)
         return self
 

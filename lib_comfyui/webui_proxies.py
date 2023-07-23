15d34e9e1a1b410510fe8c314731c02e02214fa8
ljleb <set>
2023-07-23 12:43:04 -0400

    dont forward kwargs

--- lib_comfyui/webui_proxies.py
+++ lib_comfyui/webui_proxies.py
@@ -96,8 +96,8 @@ class WebuiModelProxy:
     def apply_model(self, *args, **kwargs):
         args = torch_utils.deep_to(args, device='cpu')
         del kwargs['transformer_options']
-        kwargs = torch_utils.deep_to(kwargs, device='cpu')
-        return torch_utils.deep_to(WebuiModelProxy.sd_model_apply(*args, **kwargs), device=self.device)
+        #kwargs = torch_utils.deep_to(kwargs, device='cpu')
+        return torch_utils.deep_to(WebuiModelProxy.sd_model_apply(*args), device=self.device)
 
     @ipc.confine_to('webui')
     @staticmethod

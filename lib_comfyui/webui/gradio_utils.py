71c4aef2dd653d476a44d4babdc11c71f08b8a11
ljleb <lebel.louisjacob@gmail.com>
2023-08-04 14:06:32 -0400

    hardcode key in case of extension rename

--- lib_comfyui/webui/gradio_utils.py
+++ lib_comfyui/webui/gradio_utils.py
@@ -1,10 +1,7 @@
 import gradio as gr
 import json
-import os
-import re
-from lib_comfyui import comfyui_context
 
 
 def ExtensionDynamicProperty(key, value, *, visible=False, **kwargs):
-    extension_property = re.sub('[^a-zA-Z0-9_]', '_', os.path.basename(comfyui_context.get_webui_base_dir()))
+    extension_property = "sd_webui_comfyui"
     return gr.HTML(f'<div {extension_property}_key="{key}">{json.dumps(value)}</div>', visible=visible, **kwargs)

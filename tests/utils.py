5ed6c4c06db3afb6579544be59d01765dadf4b88
ljleb <lebel.louisjacob@gmail.com>
2023-08-06 07:00:19 -0400

    fix tests

--- tests/utils.py
+++ tests/utils.py
@@ -6,7 +6,3 @@ def setup_test_env():
     extension_root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     if extension_root not in sys.path:
         sys.path.append(extension_root)
-
-    webui_root = os.path.dirname(os.path.dirname(extension_root))
-    if webui_root not in sys.path:
-        sys.path.append(webui_root)

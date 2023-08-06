c6c910b708f94b00d3888f91eb59ad3717a22f9a
ljleb <lebel.louisjacob@gmail.com>
2023-08-06 18:39:51 -0400

    LETSGO

--- lib_comfyui/comfyui/pre_main.py
+++ lib_comfyui/comfyui/pre_main.py
@@ -1,5 +1,4 @@
 import atexit
-import signal
 import builtins
 import sys
 import os
@@ -18,31 +17,24 @@ def comfyui_print(*args, **kwargs):
     return original_print('[ComfyUI]', *args, **kwargs)
 
 
-def main(comfyui_path, cli_args):
+def main():
     builtins.print = comfyui_print
     ipc.current_callback_listeners = {'comfyui': parallel_utils.CallbackWatcher(ipc.call_fully_qualified, 'comfyui')}
     ipc.current_callback_proxies = {'webui': parallel_utils.CallbackProxy('webui')}
     ipc.current_process_id = 'comfyui'
     atexit.register(ipc.stop_callback_listeners)
     ipc.start_callback_listeners()
-
-    def sigint_handler(sig, frame):
-        exit()
-
-    signal.signal(signal.SIGINT, sigint_handler)
-    start_comfyui(comfyui_path, cli_args)
+    start_comfyui()
 
 
 @ipc.restrict_to_process('comfyui')
-def start_comfyui(comfyui_path, cli_args):
-    sys.path.insert(0, comfyui_path)
-    sys.argv[1:] = cli_args
-
+def start_comfyui():
     print('[sd-webui-comfyui]', 'Injecting custom extensions...')
     paths.share_webui_folder_paths()
     patch_comfyui()
     print('[sd-webui-comfyui]', f'Launching ComfyUI with arguments: {" ".join(sys.argv[1:])}')
-    runpy.run_path(os.path.join(comfyui_path, 'main.py'), {}, '__main__')
+    comfyui_main_path = os.getenv('SD_WEBUI_COMFYUI_MAIN')
+    runpy.run_path(os.path.join(comfyui_main_path, 'main.py'), {}, '__main__')
 
 
 @ipc.restrict_to_process('comfyui')
@@ -50,3 +42,7 @@ def patch_comfyui():
     custom_extension_injector.register_webui_extensions()
     routes_extension.patch_server_routes()
     queue_tracker.patch_prompt_queue()
+
+
+if __name__ == '__main__':
+    main()
--- lib_comfyui/comfyui_process.py
+++ lib_comfyui/comfyui_process.py
@@ -1,11 +1,13 @@
 import atexit
-import signal
+import inspect
 import os
+import signal
+import subprocess
+import sys
 from torch import multiprocessing
 from lib_comfyui import ipc, torch_utils, argv_conversion, parallel_utils
 from lib_comfyui.webui import settings
 from lib_comfyui.comfyui import pre_main
-from lib_comfyui.comfyui_context import ComfyuiContext
 
 
 comfyui_process = None
@@ -16,11 +18,11 @@ multiprocessing_spawn = multiprocessing.get_context('spawn')
 def start():
     from modules import shared
 
-    install_location = settings.get_install_location()
-    if not os.path.exists(install_location):
+    if not getattr(shared.opts, 'comfyui_enabled', True):
         return
 
-    if not getattr(shared.opts, 'comfyui_enabled', True):
+    install_location = settings.get_install_location()
+    if not os.path.exists(install_location):
         return
 
     ipc.current_callback_listeners = {'webui': parallel_utils.CallbackWatcher(ipc.call_fully_qualified, 'webui')}
@@ -30,19 +32,25 @@ def start():
     start_comfyui_process(install_location)
 
 
+@ipc.restrict_to_process('webui')
 def start_comfyui_process(install_location):
     global comfyui_process
 
-    with ComfyuiContext():
-        comfyui_process = multiprocessing_spawn.Process(
-            target=pre_main.main,
-            args=(
-                install_location,
-                argv_conversion.get_comfyui_args(),
-            ),
-            daemon=True,
-        )
-        comfyui_process.start()
+    comfyui_env = os.environ.copy()
+    comfyui_env['SD_WEBUI_COMFYUI_MAIN'] = install_location
+    python_path = comfyui_env['PYTHONPATH'].split(os.pathsep)
+    python_path.insert(1, os.path.dirname(os.path.dirname(__file__)))
+    python_path.insert(1, install_location)
+    comfyui_env['PYTHONPATH'] = os.pathsep.join(python_path)
+
+    args = [sys.executable, inspect.getfile(pre_main)] + argv_conversion.get_comfyui_args()
+
+    comfyui_process = subprocess.Popen(
+        args=args,
+        executable=sys.executable,
+        creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,
+        env=comfyui_env,
+    )
 
 
 def stop():
@@ -57,14 +65,21 @@ def stop_comfyui_process():
     if comfyui_process is None:
         return
 
-    comfyui_process.terminate()
+    stop_comfyui_main_thread()
+    comfyui_process.join()
     comfyui_process = None
+
+
+@ipc.run_in_process('comfyui')
+def stop_comfyui_main_thread():
+    import _thread
+    _thread.interrupt_main()
 
 
 # remove this when comfyui starts using subprocess with an isolated venv
 def restore_webui_sigint_handler():
-    print('[sd-webui-comfyui]', 'restoring graceful SIGINT handler for the webui process')
     def sigint_handler(sig, frame):
         exit()
 
+    print('[sd-webui-comfyui]', 'restoring graceful SIGINT handler for the webui process')
     signal.signal(signal.SIGINT, sigint_handler)

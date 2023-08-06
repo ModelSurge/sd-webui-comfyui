e71e5bfff0d6499c28db4a1bb30ea947ca877443
ljleb <lebel.louisjacob@gmail.com>
2023-08-06 15:00:20 -0400

    progress

--- lib_comfyui/parallel_utils.py
+++ lib_comfyui/parallel_utils.py
@@ -1,8 +1,10 @@
 import hashlib
+import multiprocessing.shared_memory
 import pickle
 import tempfile
 import threading
 from pathlib import Path
+from typing import Optional, Any
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -43,17 +45,14 @@ class CallbackQueue:
 
     def start(self):
         self._lock.start()
-        with self._lock:
-            self._consumer_ready_event.start()
-            self._args_payload.start()
-            self._res_payload.start()
+        self._consumer_ready_event.start()
+        self._res_payload.start()
+        self._args_payload.start()
 
     def stop(self):
-        with self._lock:
-            self._args_payload.stop()
-            self._res_payload.stop()
-            self._consumer_ready_event.stop()
-
+        self._args_payload.stop()
+        self._res_payload.stop()
+        self._consumer_ready_event.stop()
         self._lock.stop()
 
     def attend_consumer(self, callback, timeout: float = None):
@@ -106,47 +105,55 @@ class StoppableThread(threading.Thread):
 
 
 class IpcPayload:
-    def __init__(self, name, payload_directory=None):
-        self._payload_directory = tempfile.gettempdir() if payload_directory is None else payload_directory
-        unique_token = hashlib.sha256(name.encode()).hexdigest()
-        self._filepath = Path(self._payload_directory, 'ipcpayload-' + unique_token + '.payload')
+    def __init__(self, name):
+        self._name = name
+        self._shm = None
         self._lock = IpcLock(f"{IpcPayload.__name__}_lock_{name}")
         self._memory_event = IpcEvent(f"{IpcPayload.__name__}_memory_event_{name}")
+        self.start()
+
+    def __del__(self):
+        self.stop()
 
     def start(self):
         self._lock.start()
-        with self._lock:
-            self._memory_event.start()
-            self._filepath.unlink(missing_ok=True)
+        self._memory_event.start()
 
     def stop(self):
-        with self._lock:
-            self._memory_event.start()
-
+        self._memory_event.stop()
         self._lock.stop()
 
-    def send(self, value: object):
+    def send(self, value: Any):
         with self._lock:
-            self._filepath.unlink(missing_ok=True)
-            with RestoreTorchLoad():
-                data = pickle.dumps(value)
+            data = pickle.dumps(value)
+            if self._shm:
+                self._shm.close()
+                self._shm.unlink()
 
-            with open(str(self._filepath), 'bx') as f:
-                f.write(data)
-
+            self._shm = multiprocessing.shared_memory.SharedMemory(f"{IpcPayload.__name__}_lock_{self._name}", create=True, size=len(data))
+            self._shm.buf[:] = data
             self._memory_event.set()
 
-    def recv(self, timeout=None) -> object:
-        self._memory_event.wait(timeout=timeout)
+    def recv(self, timeout: Optional[float] = None) -> Any:
         with self._lock:
-            with open(str(self._filepath), 'br') as f:
-                data = f.read()
+            self._memory_event.wait(timeout=timeout)
 
+            if self._shm:
+                shm_needs_unlink = True
+            else:
+                self._shm = multiprocessing.shared_memory.SharedMemory(f"{IpcPayload.__name__}_lock_{self._name}")
+                shm_needs_unlink = False
+
+            with RestoreTorchLoad():
+                value = pickle.loads(self._shm.buf)
+
+            self._shm.close()
+            if shm_needs_unlink:
+                self._shm.unlink()
+
+            self._shm = None
             self._memory_event.clear()
-            self._filepath.unlink(missing_ok=True)
-
-        with RestoreTorchLoad():
-            return pickle.loads(data)
+        return value
 
 
 class RestoreTorchLoad:
@@ -173,59 +180,88 @@ class IpcEvent:
         self._name = name
         self._event_directory = tempfile.gettempdir() if event_directory is None else event_directory
         unique_token = hashlib.sha256(name.encode()).hexdigest()
-        self._filepath = Path(self._event_directory, 'ipcevent-' + unique_token + '.event')
-        self._event_handler = None
-        self._observer = None
+        self._event_path = Path(self._event_directory, 'ipcevent-' + unique_token + '.event')
+        self._alive_path = Path(self._event_directory, 'ipclock-alive-' + unique_token + '.lifetime')
+        self._alive_file = None
+
         self._lock = IpcLock(f'{IpcEvent.__name__}_lock_{name}')
+        self._event = threading.Event()
+        self._observer = None
+
+        self.start()
+
+    def __del__(self):
+        self.stop()
 
     def start(self):
-        self._event_handler = _IpcEventFileHandler(self._filepath)
+        self._event.clear()
+        event_handler = _IpcEventFileHandler(str(self._event_path), self._event)
         self._observer = Observer()
-        self._observer.schedule(self._event_handler, path=self._event_directory, recursive=False)
+        self._observer.schedule(event_handler, path=self._event_directory, recursive=False)
         self._observer.start()
-        self._lock.start()
-        self.clear()
+
+        with self._lock:
+            try:
+                self._alive_path.unlink()
+                self._event_path.unlink(missing_ok=True)
+                with open(self._alive_path, 'x'): pass
+                self._alive_file = open(self._alive_path, 'a')
+            except PermissionError:
+                self._alive_file = open(self._alive_path, 'a')
+                if self._event_path.exists():
+                    self._event.set()
+            except FileNotFoundError:
+                self._event_path.unlink(missing_ok=True)
+                with open(self._alive_path, 'x'): pass
+                self._alive_file = open(self._alive_path, 'a')
 
     def stop(self):
-        if self._observer is None:
-            return
+        if self._alive_file is not None:
+            self._alive_file.close()
+            self._alive_file = None
 
-        self._observer.stop()
-        self._observer.join()
-        self._observer = None
+        if self._lock:
+            with self._lock:
+                try:
+                    self._alive_path.unlink(missing_ok=True)
+                except PermissionError:
+                    pass
+
+        if self._observer is not None:
+            self._observer.stop()
+            self._observer.join()
+            self._observer = None
+
         self._lock.stop()
 
     def set(self):
-        with self._lock, open(self._filepath, 'a'):
+        with self._lock, open(self._event_path, 'a'):
             pass
 
     def clear(self):
         with self._lock:
-            self._filepath.unlink(missing_ok=True)
+            self._event_path.unlink(missing_ok=True)
 
     def is_set(self):
         with self._lock:
-            return self._filepath.exists()
+            return self._event_path.exists()
 
     def wait(self, timeout=None):
-        return self._event_handler.wait_for_creation(timeout)
+        return self._event.wait(timeout)
 
 
 class _IpcEventFileHandler(FileSystemEventHandler):
-    def __init__(self, filepath):
+    def __init__(self, filepath, event):
         self._filepath = filepath
-        self._local_event = threading.Event()
+        self._event = event
 
     def on_created(self, event):
         if event.src_path == str(self._filepath):
-            self._local_event.set()
+            self._event.set()
 
     def on_deleted(self, event):
         if event.src_path == str(self._filepath):
-            self._local_event.clear()
-
-    def wait_for_creation(self, timeout=None):
-        return self._local_event.wait(timeout)
+            self._event.clear()
 
 
 class IpcLock:
@@ -233,60 +269,101 @@ class IpcLock:
         self._name = name
         self._lock_directory = tempfile.gettempdir() if lock_directory is None else lock_directory
         unique_token = hashlib.sha256(name.encode()).hexdigest()
-        self._filepath = Path(self._lock_directory, 'ipclock-' + unique_token + '.lock')
-
-        self._event = threading.Event()
-        self._event.set()
-
-        self._event_handler = None
+        self._lock_path = Path(self._lock_directory, 'ipclock-' + unique_token + '.lock')
+        self._lock_file = None
         self._observer = None
 
+        self._lock_event = threading.Event()
+        self.start()
+
+    def __del__(self):
+        self.stop()
+
     def start(self):
-        self._event_handler = _IpcLockFileHandler(str(self._filepath), self._event)
+        self._lock_event.set()
+        event_handler = _IpcLockFileHandler(str(self._lock_path), self._lock_event)
         self._observer = Observer()
-        self._observer.schedule(self._event_handler, path=self._lock_directory, recursive=False)
+        self._observer.schedule(event_handler, path=self._lock_directory, recursive=False)
         self._observer.start()
-        self.release()
+
+        # collapse system-wide state and align events
+        if self.is_locked():
+            # close the lock if it was accidentally left locked by previous usage
+            try:
+                # if no other process has the file open, collapse the state
+                # missing_ok=True for the case where another process was holding a reference but closed it since then
+                self._lock_path.unlink(missing_ok=True)
+            except PermissionError:
+                # another process is holding the lock, so the state is valid
+                # safely call self._lock_event.clear() using the fs observer
+                with self:
+                    pass
 
     def stop(self):
-        if self._observer is None:
+        if not self.is_started():
             return
 
         self._observer.stop()
         self._observer.join()
         self._observer = None
 
+    def is_started(self):
+        return self._observer is not None
+
+    def is_locked(self):
+        if self.is_acquired():
+            return True
+
+        try:
+            with open(self._lock_path, 'x'):
+                pass
+            self.release()
+            return False
+        except FileExistsError:
+            return True
+
+    def is_acquired(self):
+        return self._lock_file is not None
+
     def acquire(self, timeout=None):
         while True:
-            if self._event.wait(timeout):
+            if self._lock_event.wait(timeout):
                 try:
-                    with open(self._filepath, 'x'):
-                        self._event.clear()
-                        return True
-                except FileExistsError:
+                    self._lock_file = open(self._lock_path, 'x')
+                    return True
+                except (FileExistsError, PermissionError):
                     continue
             else:
                 return False
 
     def release(self):
-        self._filepath.unlink(missing_ok=True)
+        if self._lock_file is not None:
+            self._lock_file.close()
+            self._lock_file = None
+        while True:
+            try:
+                self._lock_path.unlink(missing_ok=True)
+                return
+            except PermissionError:
+                continue
 
     def __enter__(self):
-        return self.acquire()
+        self.acquire()
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         return self.release()
 
 
 class _IpcLockFileHandler(FileSystemEventHandler):
-    def __init__(self, filepath, event):
+    def __init__(self, filepath, lock_event):
         self._filepath = filepath
-        self._event = event
+        self._lock_event = lock_event
 
     def on_created(self, event):
-        if event.src_path == str(self._filepath):
-            self._event.clear()
+        if event.src_path == self._filepath:
+            self._lock_event.clear()
 
     def on_deleted(self, event):
-        if event.src_path == str(self._filepath):
-            self._event.set()
+        if event.src_path == self._filepath:
+            self._lock_event.set()
---
+++ tests/lib_comfyui_tests/parallel_utils_tests/ipc_lock_test.py
@@ -0,0 +1,37 @@
+import time
+import unittest
+from tests.utils import setup_test_env
+setup_test_env()
+from lib_comfyui.parallel_utils import IpcLock, IpcEvent
+import pathlib
+log_path = pathlib.Path(__file__).parent / 'logs.txt'
+with open(log_path, 'w'):
+    pass
+
+
+from multiprocessing import Process, Value, Array
+from time import sleep
+
+
+def log(*message):
+    with open(log_path, 'a') as log_file:
+        log_file.write(" ".join(str(v) for v in message) + "\n")
+        log_file.flush()
+
+
+class TestIpcLock(unittest.TestCase):
+    def test_acquire_timeout_waits(self):
+        lock = IpcLock('test')
+        timeout = 1
+        with IpcLock('test'):
+            start = time.time()
+            lock.acquire(timeout=timeout)
+            diff = time.time() - start
+
+        self.assertGreaterEqual(diff, timeout)
+
+    def test_
+
+
+if __name__ == '__main__':
+    unittest.main()

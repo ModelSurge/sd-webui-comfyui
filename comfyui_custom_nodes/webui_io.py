bafc4de3203ca50533b61698b2f8e8ff90199862
ljleb <lebel.louisjacob@gmail.com>
2023-08-05 23:16:27 -0400

    use comfyui latent format

--- comfyui_custom_nodes/webui_io.py
+++ comfyui_custom_nodes/webui_io.py
@@ -1,5 +1,6 @@
 import torch
 from lib_comfyui import global_state
+from lib_comfyui.webui.proxies import get_comfy_model_config
 from lib_comfyui.comfyui.webui_io import NODE_DISPLAY_NAME_MAPPINGS
 
 class WebuiImageInput:
@@ -41,14 +42,6 @@ class WebuiImageOutput:
         return []
 
 
-def get_sd_model_multiplier():
-    from lib_comfyui.webui.proxies import sd_model_get_config
-    import yaml
-    with open(sd_model_get_config(), 'r') as yaml_file:
-        config = yaml.safe_load(yaml_file)
-        return config['model']['params']['scale_factor']
-
-
 class WebuiLatentInput:
     @classmethod
     def INPUT_TYPES(cls):
@@ -63,8 +56,8 @@ class WebuiLatentInput:
     CATEGORY = "webui"
 
     def get_latents(self, void):
-        multiplier = get_sd_model_multiplier()
-        return {'samples': global_state.node_inputs / multiplier},
+        latent_format = get_comfy_model_config().latent_format
+        return {'samples': latent_format.process_out(global_state.node_inputs)},
 
 
 class WebuiLatentOutput:
@@ -83,8 +76,8 @@ class WebuiLatentOutput:
     OUTPUT_NODE = True
 
     def set_images(self, latents):
-        multiplier = get_sd_model_multiplier()
-        global_state.node_outputs += latents['samples'].to('cpu') * multiplier
+        latent_format = get_comfy_model_config().latent_format
+        global_state.node_outputs += latent_format.process_in(latents['samples'].to('cpu'))
         return []
 
 

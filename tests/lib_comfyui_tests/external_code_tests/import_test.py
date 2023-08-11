e37cee623bb2d23573e08ecce049b8acf3284224
ljleb <lebel.louisjacob@gmail.com>
2023-08-11 00:14:00 -0400

    test

---
+++ tests/lib_comfyui_tests/external_code_tests/import_test.py
@@ -0,0 +1,19 @@
+import sys
+import unittest
+from tests.utils import setup_test_env
+setup_test_env()
+import importlib
+from lib_comfyui import external_code
+
+
+class ImportlibTest(unittest.TestCase):
+    def test_importlib_uses_modules_cache(self):
+        WorkflowType = external_code.WorkflowType
+        del sys.modules['lib_comfyui.external_code']
+        comfyui_api = importlib.import_module('lib_comfyui.external_code', 'external_code')
+
+        self.assertIs(WorkflowType, comfyui_api.WorkflowType)
+
+
+if __name__ == '__main__':
+    unittest.main()

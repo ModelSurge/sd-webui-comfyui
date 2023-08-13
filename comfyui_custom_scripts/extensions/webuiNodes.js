abcf4d2c1e9f89e57691a37d9f1839918cd022e4
ljleb <lebel.louisjacob@gmail.com>
2023-08-13 19:48:14 -0400

    links type

--- comfyui_custom_scripts/extensions/webuiNodes.js
+++ comfyui_custom_scripts/extensions/webuiNodes.js
@@ -26,24 +26,6 @@ app.registerExtension({
             },
         };
     },
-    async nodeCreated(node) {
-        let iframeInfo = null;
-        try {
-            iframeInfo = await iframeRegisteredEvent;
-        } catch {
-            return;
-        }
-
-        if (!iframeInfo.webuiIoNodeNames.includes(node.type)) {
-            return;
-        }
-
-        if (node.type.includes('From')) {
-            node.outputs[0].type = iframeInfo.webuiIoTypes.outputs[0];
-        } else if (node.type.includes('To')) {
-            node.inputs[0].type = iframeInfo.webuiIoTypes.inputs[0];
-        }
-    },
     async addCustomNodeDefs(defs) {
         let iframeInfo = null;
 
@@ -58,6 +40,40 @@ app.registerExtension({
         const nodes = iframeInfo.webuiIoNodeNames.map(name => defs[name]);
         for (const node of nodes) {
             node.display_name = `${node.display_name} - ${iframeInfo.workflowTypeDisplayName}`;
+
+            if (node.name.includes('From')) {
+                node.output[0] = iframeInfo.webuiIoTypes.outputs[0];
+            } else if (node.name.includes('To')) {
+                node.input.required.output[0] = iframeInfo.webuiIoTypes.inputs[0];
+            }
+        }
+    },
+    async nodeCreated(node) {
+        let iframeInfo = null;
+        try {
+            iframeInfo = await iframeRegisteredEvent;
+        } catch {
+            // TODO: disconnect nodes and patch their connection types to be `undefined`
+            return;
+        }
+
+        if (!iframeInfo.webuiIoNodeNames.includes(node.type)) {
+            return;
+        }
+
+        const output_type = iframeInfo.webuiIoTypes.outputs[0];
+        const input_type = iframeInfo.webuiIoTypes.inputs[0];
+
+        if (node.type.includes('From')) {
+            node.outputs[0].type = output_type;
+            if (output_type == input_type) {
+                for (const link of node.outputs[0].links) {
+                    app.graph.links[link].type = output_type;
+                }
+            }
+        } else if (node.type.includes('To')) {
+            node.inputs[0].type = input_type;
+            app.graph.links[node.inputs[0].link].type = input_type;
         }
     },
 });

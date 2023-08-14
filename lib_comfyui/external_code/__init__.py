231c90398a62080c3868515d24334e2d85340fbb
ljleb <lebel.louisjacob@gmail.com>
2023-08-14 11:19:51 -0400

    put in function

--- lib_comfyui/external_code/__init__.py
+++ lib_comfyui/external_code/__init__.py
@@ -1,8 +1,11 @@
-import sys
-from pathlib import Path
+def fix_path():
+    import sys
+    from pathlib import Path
 
-extension_path = str(Path(__file__).parent.parent.parent)
-if extension_path not in sys.path:
-    sys.path.append(extension_path)
+    extension_path = str(Path(__file__).parent.parent.parent)
+    if extension_path not in sys.path:
+        sys.path.append(extension_path)
 
+
+fix_path()
 from .api import *

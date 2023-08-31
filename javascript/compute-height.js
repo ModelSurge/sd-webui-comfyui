f4a7b26e398b4a28cd9be581daab15283fd80cc1
ljleb <lebel.louisjacob@gmail.com>
2023-08-30 21:22:46 -0400

    more unnecessary complexity

--- javascript/compute-height.js
+++ javascript/compute-height.js
@@ -90,7 +90,7 @@ function updateFooterStyle() {
 
 function getClearEnabledDisplayNamesButtons() {
     return [
-        document.getElementById("script_txt2txt_comfyui_clear_enabled_display_names") ?? null,
+        document.getElementById("script_txt2txt_comfyui_clear_enabled_display_names") ?? document.getElementById("script_txt2img_comfyui_clear_enabled_display_names") ?? null,
         document.getElementById("script_img2img_comfyui_clear_enabled_display_names") ?? null,
     ];
 }
@@ -125,9 +125,7 @@ function getExtensionDynamicProperty(key) {
 }
 
 function reloadFrameElement(iframeElement) {
-    oldSrc = iframeElement.src;
-    iframeElement.src = '';
-    iframeElement.src = oldSrc;
+    iframeElement.src += "";
 }
 
 function setupIFrame(workflowTypeId) {

7980125cb3f9a44d73e5a7066bf29d195106cf5a
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-11-05 09:02:16 -0500

    refact

--- install_comfyui.py
+++ install_comfyui.py
@@ -6,25 +6,23 @@ default_install_location = os.path.join(os.path.dirname(os.path.realpath(__file_
 
 
 def main(install_location, should_install_manager=False):
-    import git
-    git_repo_url = 'https://github.com/comfyanonymous/ComfyUI.git'
-    os.mkdir(install_location)
-    git.Repo.clone_from(git_repo_url, install_location)
+    repo_url = 'https://github.com/comfyanonymous/ComfyUI.git'
+    install_repo(repo_url, install_location)
 
     if should_install_manager:
+        manager_repo_url = 'https://github.com/ltdrdata/ComfyUI-Manager.git'
         manager_location = manager_location_from_comfyui_location(install_location)
-        install_manager(manager_location)
-
-
-def install_manager(manager_location):
-    import git
-    git_repo_url = 'https://github.com/ltdrdata/ComfyUI-Manager.git'
-    os.mkdir(manager_location)
-    git.Repo.clone_from(git_repo_url, manager_location)
+        install_repo(manager_repo_url, manager_location)
 
 
 def manager_location_from_comfyui_location(comfyui_location):
     return os.path.join(comfyui_location, 'custom_nodes', 'ComfyUI-Manager')
+
+
+def install_repo(git_repo_url, install_location):
+    import git
+    os.mkdir(install_location)
+    git.Repo.clone_from(git_repo_url, install_location)
 
 
 def update(install_location):
